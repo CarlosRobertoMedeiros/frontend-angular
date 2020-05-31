66fba7da629fdf26897050a2b4fe982bb94b6c06
henemesis <henemesis@gmail.com>
2020-05-31 12:18:45 -0300

    Implementando componente 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListaUsuarioComponent } from './lista-usuario/lista-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaUsuarioComponent
   ],
   imports: [
     BrowserModule
